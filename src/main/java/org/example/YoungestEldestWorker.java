42618a7f769eac92d66885d0ac5145d7dcdc0821
Saw535 <Saw.535.rud@gmail.com>
2024-12-29 04:54:40 +0200

    Create YoungestEldestWorker.java

---
+++ src/main/java/org/example/YoungestEldestWorker.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.Date;
+
+public class YoungestEldestWorker {
+    private String type;
+    private String name;
+    private Date birthday;
+
+    public YoungestEldestWorker(String type, String name, Date birthday) {
+        this.type = type;
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "YoungestEldestWorker{" +
+                "type='" + type + '\'' +
+                ", name='" + name + '\'' +
+                ", birthday=" + birthday +
+                '}';
+    }
+}
